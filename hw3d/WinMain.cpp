dd73028d8eb99b00692a35833c328a1e991b90b7
Chili <shaolj2@lenovo.com>
2025-10-13 16:30:26 +0800

    打印WindowsMessage消息

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd,msg,wParam,lParam);
+}
+
 int CALLBACK WinMain(
 	 HINSTANCE hInstance,
 	 HINSTANCE hPrevInstance,
@@ -13,7 +24,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -28,9 +39,22 @@ int CALLBACK WinMain(
 	
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true)
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0) )> 0)
 	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+		
+	
 
-	return 0;
 }
