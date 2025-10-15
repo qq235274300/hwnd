ada99cae41bc76c3bed04fe4d52ef20667b8c8d1
Chili <shaolj2@lenovo.com>
2025-10-15 10:55:57 +0800

    WM_CHAR / Mouse]

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,11 +1,51 @@
 #include <Windows.h>
+#include "WindowsMessageMap.h"
+#include <sstream>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	std::string wndMessage = mm(msg, lParam, wParam).c_str();
+	int len = MultiByteToWideChar(CP_UTF8, 0, wndMessage.c_str(), -1, nullptr, 0);
+	std::wstring wMsg(len, L'\0');
+	MultiByteToWideChar(CP_UTF8, 0, wndMessage.c_str(), -1, &wMsg[0], len);
+	OutputDebugString(wMsg.c_str());
+
 	switch (msg)
 	{
 	case WM_CLOSE:
 		PostQuitMessage(69);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd,L"No Respect");
+		}
+		break;
+
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd,L"Respect");
+		}
+		break;
+
+	case WM_CHAR:
+		{
+			static std::wstring typeText;
+			typeText.push_back((char)wParam);
+			SetWindowText(hWnd, typeText.c_str());
+			break;
+		}	
+	case WM_LBUTTONDOWN:
+		POINTS pt = MAKEPOINTS(lParam);
+		std::ostringstream oss;
+		oss << "(" <<pt.x << "," << pt.y << ")";
+		int len = MultiByteToWideChar(CP_UTF8, 0, oss.str().c_str(), -1, nullptr, 0);
+		std::wstring wMsg(len, L'\0');
+		MultiByteToWideChar(CP_UTF8, 0, oss.str().c_str(), -1, &wMsg[0], len);
+		SetWindowText(hWnd, wMsg.c_str());
 		break;
 	}
 	return DefWindowProc(hWnd,msg,wParam,lParam);
