b55f3d17b358475570ac0f7c618b4bf117ed7308
Chili <shaolj2@lenovo.com>
2025-11-20 14:38:19 +0800

    添加SkinnedBox

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -4,6 +4,7 @@
 #include "Melon.h"
 #include "Pyramid.h"
 #include "Box.h"
+#include "SkinnedBox.h"
 #include "Sheet.h"
 #include <memory>
 #include <algorithm>
@@ -46,6 +47,11 @@ App::App(): wnd(800, 600, wndName)
 					gfx, rng, adist, ddist,
 					odist, rdist
 				);
+			case 4:
+				return std::make_unique<SkinnedBox>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+				);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -61,7 +67,7 @@ App::App(): wnd(800, 600, wndName)
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,3 };
+		std::uniform_int_distribution<int> typedist{ 0,4 };//生成随机值
 	};
 
 	Factory f(wnd.Gfx());
